9a5007e8fec8bf103cfa319f4fb7d2540d896cd2
sukanda237 <sukandawiguna@mail.unpas.ac.id>
2023-08-15 07:23:15 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// modul bawaan dari node js untuk menuntukan direktori atau pengalamat folder supaya tidak terjadi error
+const path = require('path');
+// memanggil library express js kedalam project kita 
+const express = require('express');
+
+// membuat object dari express js 
+const app = express();
+
+// untuk mengakses view engine ejs pada express js 
+app.set('view engine','ejs');
+
+// menggabungankan path project kita 
+// app.set('view', path.join(__dirname, '/views'));
+// contoh pathnya adalah : sukandawiguna@Sukandas-MacBook-Pro express-ejs % (pada macbook)
+
+// membuat router baru dengan menggunkan path parameter, untuk apapunyang di inputkan oleh user di url, ambil data nya 
+app.get('/t/:tag', (req, res) => {
+
+    //membuat variabel tag dan isinya di dapat dari req params yang di inputkan oleh user
+    const {tag} = req.params;
+
+    res.render('tag', {tag});
+});
+
+
+
+// membuat router dimana ketika halaman di akses akan menampilkan file home.ejs yang ada didalam folder views 
+app.get('/', (req,res) => {
+    res.render('home');
+} );
+
+app.get('/rand', (req, res) => {
+
+    // membuat variabel yang akan di kirim ke view 
+    const math = Math.floor(Math.random() * 10) + 1;
+
+    // merender view dengan membawa objek dengan key random dan value dari variabel math 
+    res.render('random', {random: math});
+
+    // atau bisa juga dengan cara ini 
+    // res.render('random', {math});
+
+});
+
+// membuat localhost atau port yang akan kita gunakan 
+app.listen(8080, () => {
+    console.log(`Listening on host http://localhost:8080`);
+});
