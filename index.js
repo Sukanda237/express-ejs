285c2f01a908a5f2b89263969535dbdbc045182f
sukanda237 <sukandawiguna@mail.unpas.ac.id>
2023-08-16 07:39:01 +0700

    menambahakan struktur kontrol

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const express = require('express');
 // membuat object dari express js 
 const app = express();
 
+// memanggil data json kedalam index kalau di php ini merupakan include 
+const tagsData = require('./data.json');
+
 // untuk mengakses view engine ejs pada express js 
 app.set('view engine','ejs');
 
@@ -13,13 +16,30 @@ app.set('view engine','ejs');
 // app.set('view', path.join(__dirname, '/views'));
 // contoh pathnya adalah : sukandawiguna@Sukandas-MacBook-Pro express-ejs % (pada macbook)
 
+// untuk mengakses file static yang kita miliki seperti CSS, foto dll 
+app.use(express.static('public'));
+
 // membuat router baru dengan menggunkan path parameter, untuk apapunyang di inputkan oleh user di url, ambil data nya 
 app.get('/t/:tag', (req, res) => {
+
+    // console.log(tagsData); 
 
     //membuat variabel tag dan isinya di dapat dari req params yang di inputkan oleh user
     const {tag} = req.params;
 
-    res.render('tag', {tag});
+    // membuat variabel data dan di isi dengan tag data file data.json, [tag]->merupakan inputan dari user 
+    const data = tagsData[tag];
+
+    if (data) {
+    
+        res.render('tag', {data});
+        
+    }else{
+    
+        res.render('notfound', {tag});
+         
+    }
+
 });
 
 
@@ -31,10 +51,12 @@ app.get('/', (req,res) => {
 
 app.get('/cats',(req, res) => {
 
+    // membuat variabel yang tipe datanya array 
     const cats = [
         'embul','unyil','usro', 'timi', 'ujang' 
     ]
 
+    // {cats} merupakan variabel yang akan di kirim atau di render di view mencakup semua tipe data 
     res.render('cats',{ cats });
 });
 
